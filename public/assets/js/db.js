68d00b630e21fea5470a1766c58bba97b580e285
Saiban Hussein <shhussein@m.marywood.edu>
2021-11-14 15:06:38 -0600

    a

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -1,37 +1,47 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
 let db;
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function (event) {
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+request.onupgradeneeded = (event) => {
+  event.target.result.createObjectStore("pending", {
+    keyPath: "id",
+    autoIncrement: true
+  });
 };
 
-request.onsuccess = function (event) {
+request.onerror = (err) => {
+  console.log(err.message);
+};
+
+request.onsuccess = (event) => {
   db = event.target.result;
 
-  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
 };
 
-request.onerror = function (event) {
-  console.log("Woops! " + event.target.errorCode);
-};
-
+// This function is called in index.js
+// when the user creates a transaction while offline.
 function saveRecord(record) {
-  const transaction = db.transaction(["pending"], "readwrite");
+  const transaction = db.transaction("pending", "readwrite");
   const store = transaction.objectStore("pending");
-
   store.add(record);
 }
 
+// called when user goes online to send transactions stored in db to server
 function checkDatabase() {
-  const transaction = db.transaction(["pending"], "readwrite");
+  const transaction = db.transaction("pending", "readonly");
   const store = transaction.objectStore("pending");
   const getAll = store.getAll();
 
-  getAll.onsuccess = function () {
+  getAll.onsuccess = () => {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
         method: "POST",
@@ -41,20 +51,14 @@ function checkDatabase() {
           "Content-Type": "application/json"
         }
       })
-        .then(response => response.json())
+        .then((response) => response.json())
         .then(() => {
-          // delete records if successful
-          const transaction = db.transaction(["pending"], "readwrite");
+          const transaction = db.transaction("pending", "readwrite");
           const store = transaction.objectStore("pending");
           store.clear();
         });
     }
   };
-}
-function deletePending() {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-  store.clear();
 }
 
 // listen for app coming back online
